9a3ac2cd4220ae3bd9041ef25aa626b1bfe4e468
Facundo Herrera <facundhfed@gmail.com>
2023-11-17 23:14:30 +0100

    Working with server and client components

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,5 +1,22 @@
-import Image from 'next/image'
 import React from 'react'
+import Image from 'next/image'
+import { IoBrowsersOutline, IoCalculator, IoLogoReact } from 'react-icons/io5'
+import { SideBarMenuItem } from './SideBarMenuItem'
+
+const menuItems = [
+  {
+    path: '/dashboard/main',
+    icon: <IoBrowsersOutline size={40} />,
+    title: 'Dashboard',
+    subtitle: "Visualizations"
+  },
+  {
+    path: '/dashboard/counter',
+    icon: <IoCalculator size={40} />,
+    title: 'Counter',
+    subtitle: "Counter Analysis"
+  },
+]
 
 export const SiderBar = () => {
   return (
@@ -8,14 +25,18 @@ export const SiderBar = () => {
       style={{ width: '400px' }}
       className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64  left-0 h-screen ">
       <div id="logo" className="my-4 px-6">
-        <h1 className="text-lg md:text-2xl font-bold text-white">Dash<span className="text-blue-500">8</span>.</h1>
-        <p className="text-slate-500 text-sm">Manage your actions and activities</p>
+        <h1 className=" flex items-center justify-center gap-2 text-lg md:text-2xl font-bold text-white">
+          <IoLogoReact />
+          <span>Dash</span>
+          <span className="text-blue-500">8</span>
+        </h1>
+        <p className="text-slate-500 text-sm text-center">Manage your actions and activities</p>
       </div>
       <div id="profile" className="px-6 py-10">
-        <p className="text-slate-500">Welcome back,</p>
+        <p className="text-slate-500">Welcome back</p>
         <a href="#" className="inline-flex space-x-2 items-center">
           <span>
-            <Image className="rounded-full w-8 h-8" width={50} height={50} src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=128&q=80" alt="alt img" />
+            <Image className="rounded-full w-8 h-8" width={50} height={50} src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c" alt="alt img" />
           </span>
           <span className="text-sm md:text-base font-bold">
             Facundo Herrera
@@ -25,19 +46,15 @@ export const SiderBar = () => {
 
       <div id="nav" className="w-full px-6">
 
-        <a href="#" className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 bg-blue-800 hover:bg-white/5 transition ease-linear duration-150">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-white">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
-            </svg>
+        {
+          menuItems.map((menuItem) => (
 
-          </div>
-          <div className="flex flex-col">
-            <span className="text-lg font-bold leading-5 text-white">Dashboard</span>
-            <span className="text-sm text-white/50 hidden md:block">Data Overview</span>
-          </div>
-        </a>
+            <SideBarMenuItem key={menuItem.title} {...menuItem} />
 
+          ))
+        }
+
+        {/* 
         <a href="#" className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 hover:bg-white/5 transition ease-linear duration-150">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
@@ -48,7 +65,7 @@ export const SiderBar = () => {
             <span className="text-lg text-slate-300 font-bold leading-5">Counter</span>
             <span className="text-sm text-slate-500 hidden md:block">Local State</span>
           </div>
-        </a>
+        </a> */}
 
 
 
--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,18 @@
+import { CartCounter } from "@/app/components/shopping-cart";
 
+
+
+export const metadata = {
+    title: 'Shopping cart',
+    description: 'Simple shopping cart'
+}
 export default function CounterPage() {
+
+
     return (
-        <div>
-            <h1>Counter Page</h1>
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <span>Productos carrito de compras</span>
+            <CartCounter value={20} />
         </div>
     );
 }
